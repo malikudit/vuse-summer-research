f8b78be26593c1adc71915d1d3caef63e19dbee0
Udit Malik <udit.malik@vanderbilt.edu>
2021-07-13 22:32:15 -0500

    service

--- crudapp/src/main/java/com/udit/crudapp/service/WorkflowManager.java
+++ crudapp/src/main/java/com/udit/crudapp/service/WorkflowManager.java
@@ -4,25 +4,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WorkflowManager {
 
     public static void main(String[] args) throws Exception {
         Scanner userInputCommand = new Scanner(System.in);
-        System.out.println("Enter workflow command! Options include submit, terminate, list, and suspend.");
-    
-        String userCommand = userInputCommand.nextLine(); 
-        System.out.println("The selected command is: " + userCommand); 
 
-        String workflowName = userInputCommand.nextLine();
-        System.out.println("The selected workflow is: " + workflowName);
+        String userCommand = null, workflowName = null, checkInput = null, finalCommand = null;
+        System.out.println("Enter workflow command! Options include submit, terminate, list, and suspend.");
+        userCommand = userInputCommand.nextLine(); 
+
+        boolean check = true;
+
+        ArrayList<String> workflows = new ArrayList<String>();
+
+        while (check == true) {
+            System.out.println("The selected command is: " + userCommand); 
+
+            System.out.println("Enter workflow name: ");
+            workflowName = userInputCommand.nextLine();
+            System.out.println("The selected workflow is: " + workflowName);
+            workflows.add(workflowName);
+
+            System.out.println("Do you want to add another workflow? y/n");
+            checkInput = userInputCommand.nextLine();
+            if (checkInput == "y" || checkInput == "Y") {
+                check = true;
+            }
+            else {
+                check = false;
+            }
+        }
 
         userInputCommand.close();
 
-        String finalCommand = "argo " + userCommand + " -n argo " + "http://localhost:8080/api/download/" + workflowName + ".yml";
-        System.out.println(finalCommand);
-
-        commandSubmit(finalCommand);
+        for (int i=0; i<workflows.size(); i++) {
+            if (userCommand.equals("submit")) {
+                finalCommand = "argo " + userCommand + " -n argo " + "http://localhost:8080/api/download/" + workflows.get(i) + ".yml";
+            }
+            else {
+                finalCommand = "argo " + userCommand + "-n argo " + workflows.get(i);
+            }
+            System.out.println(finalCommand);
+            commandSubmit(finalCommand);
+        }
     }
 
     public static void commandSubmit(String finalCommand) {
@@ -58,22 +84,4 @@ public class WorkflowManager {
             e.printStackTrace();
         }
     }
-
-    public String workflowSubmit() {
-        return "argo submit -n argo" + " ";
-    }
-
-    public String workflowList() {
-        return "argo list -n argo";
-    }
-
-    public String workflowTerminate() {
-        return "argo terminate -n argo" + "";
-    }
-
-    public String workflowSuspend() {
-        return "argo suspend -n argo" + "";
-    }
-
-
 }
