5664abf4d58da0e67fb430722a2a7766b35b757f
Udit Malik <udit.malik@vanderbilt.edu>
2021-07-19 16:45:02 -0500

    change column type

--- crudapp/src/main/java/com/udit/crudapp/model/Workflow.java
+++ crudapp/src/main/java/com/udit/crudapp/model/Workflow.java
@@ -13,7 +13,7 @@ public class Workflow {
 	@Column(name = "name")
 	private String name;
 
-	@Column(name = "description")
+	@Column(name = "description", columnDefinition = "text")
 	private String description;
 
 	@Column(name = "completed")
